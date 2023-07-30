35e3f54d642712e86715bcb0bcc7be74698d7727
0prrr <oprops101@gmail.com>
2023-07-30 19:59:03 +0800

    Add helper script.

--- CustomShellcode/MetTheStager/MetTheStagerHttps/srv_uri_2_bytes.py
+++ CustomShellcode/MetTheStager/MetTheStagerHttps/srv_uri_2_bytes.py
@@ -16,21 +16,21 @@ def reverse(string):
 
 s = 'LM56LbrVoq9HXkZcI5rSlg1Nj-Km_Tvk_sLa7tsF3atxHRe_FrJrIEtDeZfulWKDX0Ujm3ggJW8CYCwRNzSIaMlj6K0RW8jEHCr7p2wXkyj3UhGhneiH3LA3AOXoCViZvcEH0ThP2uZ0PtuHheJfGu457y2-Aw-tTaBSeP7lnBLL3zUfFGbWr-x1LRw_o3W7u_G9sKiLXI79acVyGuvRLKMR'
 
-print(f'\n[*]Original URI string:\n\n{s}\n\n')
+print(f'\n[*]Original URI string:\n\n{s}\n')
 
-print(f'[*]Reversed URI string:\n\n{reverse(s)}\n\n')
+print(f'[*]Reversed URI string:\n\n{reverse(s)}\n')
 
-print('[*]Generated instructions:\n\n')
+print('[*]Generated instructions:\n')
 
 rs = reverse(s)
 i = 0
-l = ''
+l = '0x'
 for c in rs:
     i += 1
     l += hex(ord(c))[2:]
     if i % 8 == 0 or i == len(s):
         print(f'"\tmov rax, {l}\t\t;"')
         print('"\tpush rax\t\t\t\t;"')
-        l = ''
+        l = '0x'
 
 print(f"\n[*]Done! Total byte lines: {i // 8}\n[*]Need to clean up extra {hex(i // 8 * 8)} bytes of stack space\n")
---
+++ CustomShellcode/ip_2_bytes.py
@@ -0,0 +1,42 @@
+import sys
+
+if len(sys.argv) < 2:
+    print(f'Usage: {sys.argv[0]} <IPv4>')
+
+def reverse(string):
+    string = string[::-1]
+    return string
+
+ip = sys.argv[1]
+
+print(f'\n[*]IP address: {ip}')
+
+s_1 = ip[0:8]
+s_2 = ip[8:]
+print(f'\n[*]First part: {s_1}')
+print(f'[*]Second part: {s_2}')
+
+s_1 = reverse(s_1)
+s_2 = reverse(s_2)
+
+print(f'\n[*]First reversed part: {s_1}')
+print(f'[*]Second reversed part: {s_2}')
+
+print("\n[*]Generated instructions:")
+
+l = '0x'
+for c in s_2:
+    l += hex(ord(c))[2:]
+
+print()
+print(f'"\tmov rax, {l}\t;"')
+print('"\tpush rax\t\t\t;"')
+
+l = '0x'
+for c in s_1:
+    l += hex(ord(c))[2:]
+
+print(f'"\tmov rax, {l}\t;"')
+print('"\tpush rax\t\t\t;"')
+print()
+
